11eaaed4820e5eab8014a4f8d13fb9cbd66a8c1f
talliholic <talliholic@gmail.com>
2022-07-13 21:53:45 -0500

    First Commit-test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
+// import Header from "./partials/Header";
 
 function App() {
-  return <div className="App">The Teacher Helper</div>;
+  return (
+    <div className="App">
+      {/* <Header /> */}
+      The Teacher Helper
+    </div>
+  );
 }
 
 export default App;
---
+++ src/partials/Header.js
@@ -0,0 +1,9 @@
+// import AdSense from "@pkasila/react-adsense";
+
+// const Header = () => (
+//   <header>
+//     <AdSense client={"ca-pub-7938279531529184"} />
+//   </header>
+// );
+
+// export default Header;
